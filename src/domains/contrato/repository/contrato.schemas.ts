af67555c4bd4393348e357abbaeb8514b3d882ae
Joel Imparado <joelimparado@gmail.com>
2023-02-27 11:11:50 -0300

    Fixes Grid Contrato

--- src/domains/contrato/repository/contrato.schemas.ts
+++ src/domains/contrato/repository/contrato.schemas.ts
@@ -5,12 +5,12 @@ import { ZodUtils } from '@app/utils';
 export const getAllContratoPaginatedSchema = ZodUtils.withPagination(
   z.object({
     id: z.number(),
-    nroContrato: z.number(),
-    tipoContrato: z.string(),
-    modeloAcuerdo: z.string(),
-    descripcion: z.string(),
-    cliente: z.string(),
-    // fechaInicioContrato: z.string(), 
+    nroContrato: z.number().nullish(),
+    tipoContrato: z.string().nullish(),
+    modeloAcuerdo: z.string().nullish(),
+    descripcion: z.string().nullish(),
+    cliente: z.string().nullish(),
+    // fechaInicioContrato: z.string(),
     // fechaFinContrato: z.string(),
     fechaInicioContrato: z.string().nullish(), // TODO ver con Sorro si admite null o undefined
     fechaFinContrato: z.string().nullish(),
