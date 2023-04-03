57fa4fe9dc83f005067eecf130c41aad3ac7d354
Cristian Perrone <cristian.perrone@intellisite.io>
2023-04-03 14:13:15 -0300

    Added DataGrid to ProcedimientoCustom

---
+++ src/domains/procedimiento-custom/container/procedimiento-custom-datagrid/ProcedimientoCustomDataGrid.tsx
@@ -0,0 +1,92 @@
+import { useCallback, useContext, useEffect } from 'react';
+
+import { useNavigate, Outlet } from 'react-router-dom';
+
+import { Col, Row } from '@app/components';
+
+import { withBreadcrumb } from '@app/hocs';
+import { useConfirmDialog } from '@app/hooks';
+
+import { DataGrid } from '@app/pro-components';
+
+import { ProcedimientoCustomRepository } from '@domains/procedimiento-custom/repository';
+import { ProcedimientoCustomDataGridBreadcrumb } from '@domains/procedimiento-custom/constants';
+import { ProcedimientoCustomContext } from '@domains/procedimiento-custom/contexts';
+
+const ProcedimientoCustomDataGrid = () => {
+  const _navigate = useNavigate();
+
+  const { mainDataGrid } = useContext(ProcedimientoCustomContext);
+
+  const confirmDialog = useConfirmDialog();
+
+  const handleClickCreate = useCallback(() => {
+    _navigate('/procedimiento-custom/create');
+  }, [_navigate]);
+
+  const handleClickEdit = useCallback(
+    (id: number) => {
+      _navigate(`/procedimiento-custom/${id}/edit`);
+    },
+    [_navigate],
+  );
+
+  const handleClickDelete = useCallback(
+    (id: number) => {
+      confirmDialog.open({
+        message: 'Desea eliminar el registro?',
+        async onClickYes() {
+          await ProcedimientoCustomRepository.deleteProcedimientoCustomById(id);
+          confirmDialog.close();
+          mainDataGrid.reload();
+        },
+      });
+    },
+    [confirmDialog, mainDataGrid],
+  );
+
+  useEffect(() => {
+    mainDataGrid.load();
+  }, [mainDataGrid]);
+
+  return (
+    <>
+      <Row>
+        <Col md={12}>
+          <DataGrid
+            hookRef={mainDataGrid.ref}
+            columnHeads={[
+              { label: 'DENOMINACIÓN' },
+              { label: 'TIPO FUNCIÓN' },
+              { label: 'ACCIÓN' },
+              { label: 'EVENTO' },
+              { label: 'CAMPO' },
+              { label: '' },
+              { label: '' },
+            ]}
+            repositoryFunc={ProcedimientoCustomRepository.getAllProcedimientoCustomPaginated}
+            rowTemplate={row => (
+              <>
+                <td>{row.denominacion}</td>
+                <td>{row.funcion}</td>
+                <td>{row.accion}</td>
+                <td>{row.evento}</td>
+                <td>{row.eventoCampo}</td>
+                <td align='center'>
+                  <DataGrid.EditButton onClick={() => handleClickEdit(row.id)} />
+                </td>
+                <td align='center'>
+                  <DataGrid.DeleteButton onClick={() => handleClickDelete(row.id)} />
+                </td>
+              </>
+            )}
+            onClickNew={handleClickCreate}
+          />
+        </Col>
+      </Row>
+      <Outlet />
+    </>
+  );
+};
+
+export default withBreadcrumb(ProcedimientoCustomDataGrid, ProcedimientoCustomDataGridBreadcrumb);
