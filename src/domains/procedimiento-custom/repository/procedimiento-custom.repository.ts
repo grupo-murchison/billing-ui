58ac64cb8f6d31622f3fc3f9e65ecb9343ad01a3
Cristian Perrone <cristian.perrone@intellisite.io>
2023-04-03 14:17:39 -0300

    implemen ProcedimientoCustom api service

--- src/domains/procedimiento-custom/repository/procedimiento-custom.repository.ts
+++ src/domains/procedimiento-custom/repository/procedimiento-custom.repository.ts
@@ -2,15 +2,53 @@ import { from, lastValueFrom } from 'rxjs';
 
 import { RepositoryUtils } from '@app/utils';
 
+import type { DataGridRepositoryFuncParams } from '@app/pro-components';
+
 import { ProcedimientoCustomService } from '@domains/procedimiento-custom/repository/procedimiento-custom.service';
-import { getAllProcedimientoCustomAsDropdownSchema } from '@domains/procedimiento-custom/repository/procedimiento-custom.schemas';
+import {
+  getAllProcedimientoCustomPaginatedSchema,
+  getAllProcedimientoCustomAsDropdownSchema,
+} from '@domains/procedimiento-custom/repository/procedimiento-custom.schemas';
 
 class ProcedimientoCustomRepository {
+  static getAllProcedimientoCustomPaginated = async (params: DataGridRepositoryFuncParams) => {
+    const response$ = from(ProcedimientoCustomService.getAllPaginated(params)).pipe(
+      RepositoryUtils.PIPES.getResponse(),
+      RepositoryUtils.PIPES.validateWithSchema(getAllProcedimientoCustomPaginatedSchema),
+    );
+    const response = await lastValueFrom(response$);
+    return response;
+  };
+
   static getAllProcedimientoCustomAsDropdown = async () => {
     const response$ = from(ProcedimientoCustomService.getAllAsDropdown()).pipe(
       RepositoryUtils.PIPES.getResponse(),
       RepositoryUtils.PIPES.validateWithSchema(getAllProcedimientoCustomAsDropdownSchema),
     );
+    const response = await lastValueFrom(response$);
+    return response;
+  };
+
+  static getProcedimientoCustomById = async (id: string) => {
+    const response$ = from(ProcedimientoCustomService.getById(id)).pipe(RepositoryUtils.PIPES.getResponse());
+    const response = await lastValueFrom(response$);
+    return response;
+  };
+
+  static createProcedimientoCustom = async (procedimientoCustom: AnyValue) => {
+    const response$ = from(ProcedimientoCustomService.post(procedimientoCustom)).pipe(RepositoryUtils.PIPES.getResponse());
+    const response = await lastValueFrom(response$);
+    return response;
+  };
+
+  static updateProcedimientoCustom = async (procedimientoCustom: AnyValue) => {
+    const response$ = from(ProcedimientoCustomService.patch(procedimientoCustom)).pipe(RepositoryUtils.PIPES.getResponse());
+    const response = await lastValueFrom(response$);
+    return response;
+  };
+
+  static deleteProcedimientoCustomById = async (id: number) => {
+    const response$ = from(ProcedimientoCustomService.deleteById(id)).pipe(RepositoryUtils.PIPES.getResponse());
     const response = await lastValueFrom(response$);
     return response;
   };
