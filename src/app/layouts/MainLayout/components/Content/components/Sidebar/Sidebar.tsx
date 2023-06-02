3837e4be4b087db179ac32234bcae8c574e52193
Joel Imparado <joelimparado@gmail.com>
2023-06-02 15:04:02 -0300

    Inicio nueva NavBar y Drawer

--- src/app/layouts/MainLayout/components/Content/components/Sidebar/Sidebar.tsx
+++ src/app/layouts/MainLayout/components/Content/components/Sidebar/Sidebar.tsx
@@ -6,16 +6,26 @@ import { MENU_ITEMS } from '@app/layouts/MainLayout/constants';
 import { Item } from '@app/layouts/MainLayout/components/Content/components/Sidebar/components';
 
 import styles from '@app/layouts/MainLayout/components/Content/components/Sidebar/Sidebar.module.scss';
+import { Drawer } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 const Sidebar = () => {
   const { isSidebarOpen } = useContext(MainLayoutContext);
-
+  const theme = useTheme();
   return (
-    <div className={`${styles['content-sidebar']} ${isSidebarOpen ? styles['content-sidebar--open'] : ''}`}>
-      {MENU_ITEMS.map(x => (
-        <Item key={x.title} title={x.title} icon={x.icon} items={x.childs} />
-      ))}
-    </div>
+    // <Drawer
+    //   variant={'permanent'}
+    //   open={isSidebarOpen}
+    //   PaperProps={{
+    //     sx: { backgroundColor: theme.palette.primary.main },
+    //   }}
+    // >
+      <div className={`${styles['content-sidebar']} ${isSidebarOpen ? styles['content-sidebar--open'] : ''}`}>
+        {MENU_ITEMS.map(x => (
+          <Item key={x.title} title={x.title} icon={x.icon} items={x.childs} />
+        ))}
+      </div>
+    // </Drawer>
   );
 };
 
--- src/app/layouts/MainLayout/components/Header/Header.tsx
+++ src/app/layouts/MainLayout/components/Header/Header.tsx
@@ -5,34 +5,50 @@ import { MainLayoutContext } from '@app/layouts/MainLayout/contexts';
 import { MenuIcon, SearchIcon, QuestionMarkIcon, AccountCircleIcon } from '@assets/icons';
 
 import styles from '@app/layouts/MainLayout/components/Header/Header.module.scss';
+import { AppBar, CssBaseline, Toolbar, Typography } from '@mui/material';
 
 const Header = () => {
   const { toogleSidebar } = useContext(MainLayoutContext);
 
   return (
-    <div className={styles['header']}>
-      <div className={styles['sidebar-trigger']}>
+    <>
+      <div className={styles['header']}>
+        <div className={styles['sidebar-trigger']}>
+          <span onClick={toogleSidebar}>
+            <MenuIcon />
+          </span>
+        </div>
+        {/* // TODO Ajustar estilos del logo  */}
+        <div className={styles['header-container']}>
+          <div className={styles['header-brand']}>
+            <img src='/logo/logo-murchison.png' alt='LOGO' />
+          </div>
+          <div className={`${styles['icon-wrapper']} ${styles['icon-wrapper-search']}`}>
+            <SearchIcon />
+          </div>
+          <div className={`${styles['icon-wrapper']} ${styles['icon-wrapper-help']}`}>
+            <QuestionMarkIcon />
+          </div>
+          <div className={styles['user-tools']}>
+            <AccountCircleIcon />
+            <span>Nombre de Usuario</span>
+          </div>
+        </div>
+      </div>
+      {/* <CssBaseline />
+      <AppBar position='fixed' sx={{ zIndex: theme => theme.zIndex.drawer + 1 }} color='default' >
+        <Toolbar>
+        <div className={styles['sidebar-trigger']}>
         <span onClick={toogleSidebar}>
           <MenuIcon />
         </span>
       </div>
-      {/* // TODO Ajustar estilos del logo  */}
-      <div className={styles['header-container']}>
-        <div className={styles['header-brand']}>
-          <img src='/logo/logo-murchison.png' alt='LOGO' />
-        </div>
-        <div className={`${styles['icon-wrapper']} ${styles['icon-wrapper-search']}`}>
-          <SearchIcon />
-        </div>
-        <div className={`${styles['icon-wrapper']} ${styles['icon-wrapper-help']}`}>
-          <QuestionMarkIcon />
-        </div>
-        <div className={styles['user-tools']}>
-          <AccountCircleIcon />
-          <span>Nombre de Usuario</span>
-        </div>
-      </div>
-    </div>
+          <Typography variant='h6' noWrap component='div'>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar> */}
+    </>
   );
 };
 
