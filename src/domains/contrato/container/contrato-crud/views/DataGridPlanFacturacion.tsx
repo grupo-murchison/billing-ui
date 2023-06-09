d38320e64f73cc9fe0741d8910e92363d66e4629
Joel Imparado <joelimparado@gmail.com>
2023-06-09 12:24:24 -0300

    Actualizaciones Grids Contrato

--- src/domains/contrato/container/contrato-crud/views/DataGridPlanFacturacion.tsx
+++ src/domains/contrato/container/contrato-crud/views/DataGridPlanFacturacion.tsx
@@ -1,13 +1,16 @@
+import { Box } from '@mui/material';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { format, parseISO } from 'date-fns';
 
 const columns: GridColDef[] = [
-  { field: 'periodo', headerName: 'Periodo', width: 80, type: 'number' },
+  { field: 'periodo', headerName: 'Periodo', width: 100, type: 'number', headerAlign: 'center', align: 'center' },
   {
     field: 'liquidacionDesde',
     headerName: 'Desde',
     width: 160,
     type: 'Date',
+    headerAlign: 'center',
+    align: 'center',
     valueFormatter: ({ value }) => format(parseISO(value), 'dd-MM-yyyy'),
   },
   {
@@ -15,6 +18,8 @@ const columns: GridColDef[] = [
     headerName: 'Hasta',
     width: 160,
     type: 'Date',
+    headerAlign: 'center',
+    align: 'center',
     valueFormatter: ({ value }) => format(parseISO(value), 'dd-MM-yyyy'),
   },
   {
@@ -22,17 +27,19 @@ const columns: GridColDef[] = [
     headerName: 'Fecha Facturación',
     width: 160,
     type: 'Date',
+    headerAlign: 'center',
+    align: 'center',
     valueFormatter: ({ value }) => format(parseISO(value), 'dd-MM-yyyy'),
   },
-  { field: 'estado', headerName: 'Estado', type: 'string', width: 110 },
+  { field: 'estado', headerName: 'Estado', type: 'string', width: 110, headerAlign: 'center', align: 'center' },
 ];
 
 export const DataGridPlanFacturacion = ({ rows }: { id: string; rows: any }) => {
   return (
     <>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[10]} />
-      </div>
+      <Box style={{ width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[10]} autoHeight={true} />
+      </Box>
     </>
   );
 };
