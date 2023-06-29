9a7e9a138b2a5d882564b347c8a549ca34e33894
assertnano <ignaciokang@assertsolutions.com>
2023-06-29 14:10:07 -0300

    Se agregan tipografias

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -6,8 +6,11 @@ import { withPortal } from '@app/hocs';
 import { ClassNameHandlerLib } from '@libs';
 
 import './Modal.scss';
+import { Typography, useTheme } from '@mui/material';
 
 const Modal = ({ isOpen, title, onClose, children }: ModalProps) => {
+  const theme = useTheme();
+
   const className = useMemo(() => {
     return ClassNameHandlerLib.merge('modal', {
       'modal--open': isOpen,
@@ -22,7 +25,10 @@ const Modal = ({ isOpen, title, onClose, children }: ModalProps) => {
     <div className={className}>
       <div className='modal-content'>
         <div className='modal-content__header'>
-          <h1 className='mch__title'>{title}</h1>
+          {/* <h1 className='mch__title'>{title}</h1> */}
+          <Typography variant='h1' sx={{ flexGrow: 1, color: theme.palette.primary.main }}>
+            {title}
+          </Typography>
           <span className='mch__closeButton' onClick={onClose}>
             &times;
           </span>
--- src/domains/root/Root.tsx
+++ src/domains/root/Root.tsx
@@ -1,9 +1,14 @@
 import { withBreadcrumb } from '@app/hocs';
 import { BreadcrumbItem } from '@app/utils/types/withBreadcrumb.type';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 function RootLayout() {
-  return <Typography variant='h4'>Inicio</Typography>;
+  return (
+    <>
+      <Typography variant='h4'>Inicio</Typography>
+      <Button variant='contained'>Avanzar</Button>
+    </>
+  );
 }
 
 const RootBreadcrumb: BreadcrumbItem[] = [{ label: '', path: '/' }];
