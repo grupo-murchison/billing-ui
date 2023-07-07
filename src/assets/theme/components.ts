2090e5fdef08839f57edef6c06fb168b48c89890
Joel Imparado <joelimparado@gmail.com>
2023-07-07 10:09:37 -0300

    Actulizacion theme

---
+++ src/assets/theme/components.ts
@@ -0,0 +1,13 @@
+import { Components } from '@mui/material';
+
+export const components: Components = {
+  MuiButton: {
+    styleOverrides: {
+      // Name of the slot
+      root: {
+        borderRadius: '6px',
+        padding: '5px 20px',
+      },
+    },
+  },
+};
