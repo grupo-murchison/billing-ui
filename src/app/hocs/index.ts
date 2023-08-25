77bd289a55f4c4edc6fb7005b8b2b3eeb004fd50
Joel Imparado <joelimparado@gmail.com>
2023-08-25 15:18:39 -0300

    Actualizacion withBreadcrumb

--- src/app/hocs/index.ts
+++ src/app/hocs/index.ts
@@ -1,2 +1,4 @@
 export { default as withBreadcrumb } from './withBreadcrumb.hoc';
+export type { BreadcrumbItem } from './withBreadcrumb.hoc';
+
 export { default as withPortal } from './withPortal.hoc';
