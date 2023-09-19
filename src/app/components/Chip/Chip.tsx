50206249a16aa5b3bdfeabda56d287bf4ca1a3ac
lautaro <lautarogonzalez@assertsolutions.com>
2023-09-19 17:21:19 -0300

    CustomChip Component

---
+++ src/app/components/Chip/Chip.tsx
@@ -0,0 +1,33 @@
+import { Chip } from '@mui/material';
+
+type ColorType =  "default" | "primary" | "info" | "error" | "secondary" | "success" | "warning"
+
+const CustomChip = ({estado}: AnyValue): any=>{  
+
+    function setColor (text: string): ColorType{
+        let output: ColorType = 'default'
+        switch (text) {
+            case 'FACTURADO':
+              output = 'primary'
+              break;
+            case 'ABIERTO':
+              output = 'info'
+              break;
+            case 'ANULADO':
+              output = 'error'
+              break;
+        }
+        return output
+    }
+
+    return (
+      <Chip
+        label={estado}
+        variant='outlined'
+        color={setColor(estado)}
+        size='small'
+      />
+    )
+}
+
+export default CustomChip;
