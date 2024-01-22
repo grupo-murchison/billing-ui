b3960d39eb44f589f52df315ee241cd7868c6bdf
assertnano <ignaciokang@assertsolutions.com>
2024-01-22 15:50:19 -0300

    WIP on fix-tabla-dinamica-dato-dinamico: 7346912 Merge branch 'openshift' of https://github.com/grupo-murchison/billing-ui into fix-tabla-dinamica-dato-dinamico

--- src/domains/evento-campo/container/evento-campo-create/schemas/index.ts
+++ src/domains/evento-campo/container/evento-campo-create/schemas/index.ts
@@ -1,29 +1,30 @@
 import z, { ZodType } from 'zod';
+import { zodId, zodLocale } from '@app/utils/zod.util';
 
 export const EventoCampoCreateSchema: ZodType<FormDataTypeEventoCampoCreate> = z
   .object({
     codigo: z
-      .string({ required_error: 'El campo es requerido.' })
-      .min(1, { message: 'El campo es requerido.' })
-      .max(50, { message: 'Ha superado el límite de caracteres' }),
+      .string({ required_error: zodLocale.required_error })
+      .min(1, { message: zodLocale.required_error })
+      .max(50, { message: zodLocale.stringMax(250) }),
     denominacion: z
-      .string({ required_error: 'El campo es requerido.' })
-      .min(1, { message: 'El campo es requerido.' })
-      .max(250, { message: 'Ha superado el límite de caracteres' }),
+      .string({ required_error: zodLocale.required_error })
+      .min(1, { message: zodLocale.required_error })
+      .max(50, { message: zodLocale.stringMax(250) }),
     descripcion: z
-      .string({ required_error: 'El campo es requerido.' })
-      .min(1, { message: 'El campo es requerido.' })
-      .max(250, { message: 'Ha superado el límite de caracteres' }),
+      .string({ required_error: zodLocale.required_error })
+      .min(1, { message: zodLocale.required_error })
+      .max(50, { message: zodLocale.stringMax(250) }),
     campo: z
-      .string({ required_error: 'El campo es requerido.' })
-      .min(1, { message: 'El campo es requerido.' })
-      .max(250, { message: 'Ha superado el límite de caracteres' }),
-    eventoId: z.number({ required_error: 'El campo es requerido.' }),
-    tipoDatoId: z.number({ required_error: 'El campo es requerido.' }),
-    tablaDinamicaId: z.number({ required_error: 'El campo es requerido.' }).nullable(),
+      .string({ required_error: zodLocale.required_error })
+      .min(1, { message: zodLocale.required_error })
+      .max(50, { message: zodLocale.stringMax(250) }),
+    eventoId: zodId,
+    tipoDatoId: zodId,
+    tablaDinamicaId: z.number({ required_error: zodLocale.required_error }).nullable(),
   })
   .refine(schema => (schema.tipoDatoId === 4 ? !!schema.tablaDinamicaId : true), {
-    message: 'El campo es requerido.',
+    message: zodLocale.required_error,
     path: ['tablaDinamicaId'],
   });
 
