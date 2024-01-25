be3bd3aa314d7f7adf651aa36977aedcd32935a3
assertnano <112961459+assertnano@users.noreply.github.com>
2024-01-25 16:39:32 -0300

    Calculo facturacion filtros de busqueda opcionales (#136)

--- src/domains/calculo/repository/schemas/index.ts
+++ src/domains/calculo/repository/schemas/index.ts
@@ -1,3 +1,4 @@
+import { zodLocale } from '@app/utils/zod.util';
 import z from 'zod';
 
 export const ValidationSchemaEventosServicioFilters = z
@@ -18,7 +19,6 @@ export const ValidationSchemaEventosServicioFilters = z
     conceptoAcuerdoId: z.object(
       {
         value: z.number({ required_error: 'El campos es requerido.' }),
-        // code: z.string({ required_error: 'El campos es requerido.' }),
         label: z.string({ required_error: 'El campos es requerido.' }),
       },
       { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
@@ -49,21 +49,33 @@ export const ValidationSchemaFacturacionReversion = z.object({
 export type FacturacionReversionFormSchemaType = z.infer<typeof ValidationSchemaFacturacionReversion>;
 
 export const ValidationSchemaCalculoReporteFilter = z.object({
-  clienteId: z.object(
-    {
-      value: z.number({ required_error: 'El campo es requerido.' }),
-      code: z.string({ required_error: 'El campo es requerido.' }),
-      label: z.string({ required_error: 'El campo es requerido.' }),
-    },
-    { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
-  ),
-  numeroSecuenciaCalculo: z.number({
-    required_error: 'El campo es requerido.',
-    invalid_type_error: 'El campo es requerido.',
-  }),
-  nroContrato: z.number({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-  fechaHasta: z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-  fechaDesde: z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
+  clienteId: z
+    .object(
+      {
+        value: z.number({ required_error: zodLocale.required_error }),
+        code: z.string({ required_error: zodLocale.required_error }),
+        label: z.string({ required_error: zodLocale.required_error }),
+      },
+      { required_error: zodLocale.required_error, invalid_type_error: zodLocale.invalid_type_error },
+    )
+    .nullable(),
+  numeroSecuenciaCalculo: z
+    .number({
+      required_error: zodLocale.required_error,
+      invalid_type_error: zodLocale.invalid_type_error,
+    })
+    .nullable()
+    .or(z.literal('')),
+  nroContrato: z
+    .number({ required_error: zodLocale.required_error, invalid_type_error: zodLocale.invalid_type_error })
+    .nullable()
+    .or(z.literal('')),
+  fechaHasta: z
+    .date({ required_error: zodLocale.required_error, invalid_type_error: zodLocale.invalid_type_error })
+    .nullable(),
+  fechaDesde: z
+    .date({ required_error: zodLocale.required_error, invalid_type_error: zodLocale.invalid_type_error })
+    .nullable(),
 });
 
 export type FacturacionReporteFormSchemaType = z.infer<typeof ValidationSchemaCalculoReporteFilter>;
