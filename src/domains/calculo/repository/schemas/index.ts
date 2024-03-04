93d916fe0737925807897251f994c428bf47ad20
assertnano <ignaciokang@assertsolutions.com>
2024-03-04 16:54:24 -0300

    Componente rango de fechas aplicado en:
    CalculoReporte
    CalculoLog
    CalculoReversion
    EventosServicios
    ClienteEventos

--- src/domains/calculo/repository/schemas/index.ts
+++ src/domains/calculo/repository/schemas/index.ts
@@ -1,35 +1,31 @@
 import { zodLocale } from '@app/utils/zod.util';
 import z from 'zod';
 
-export const ValidationSchemaEventosServicioFilters = z
-  .object({
-    numeroSecuenciaCalculo: z.number({
-      required_error: 'El campo es requerido.',
-      invalid_type_error: 'El campo es requerido.',
-    }),
-    clienteId: z.object(
-      {
-        value: z.number({ required_error: 'El campo es requerido.' }),
-        code: z.string({ required_error: 'El campo es requerido.' }),
-        label: z.string({ required_error: 'El campo es requerido.' }),
-      },
-      { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
-    ),
-    contrato: z.string({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-    conceptoAcuerdoId: z.object(
-      {
-        value: z.number({ required_error: 'El campos es requerido.' }),
-        label: z.string({ required_error: 'El campos es requerido.' }),
-      },
-      { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
-    ),
-    fechaHasta: z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-    fechaDesde: z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-  })
-  .refine(({ fechaDesde, fechaHasta }) => fechaHasta == null || fechaDesde == null || fechaHasta >= fechaDesde, {
-    message: 'Fecha hasta debe ser mayor o igual a Fecha desde',
-    path: ['fechaHasta'],
-  });
+export const ValidationSchemaEventosServicioFilters = z.object({
+  numeroSecuenciaCalculo: z.number({
+    required_error: 'El campo es requerido.',
+    invalid_type_error: 'El campo es requerido.',
+  }),
+  clienteId: z.object(
+    {
+      value: z.number({ required_error: 'El campo es requerido.' }),
+      code: z.string({ required_error: 'El campo es requerido.' }),
+      label: z.string({ required_error: 'El campo es requerido.' }),
+    },
+    { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
+  ),
+  contrato: z.string({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
+  conceptoAcuerdoId: z.object(
+    {
+      value: z.number({ required_error: 'El campos es requerido.' }),
+      label: z.string({ required_error: 'El campos es requerido.' }),
+    },
+    { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
+  ),
+  rangoFechas: z
+    .array(z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }))
+    .nullable(),
+});
 
 export const ValidationSchemaFacturacionReversion = z.object({
   clienteId: z.object(
--- src/domains/evento-cliente/schemas/index.ts
+++ src/domains/evento-cliente/schemas/index.ts
@@ -1,30 +1,26 @@
 import z from 'zod';
 
-export const EventosClientesCreateSchema = z
-  .object({
-    clienteId: z.object(
-      {
-        value: z.number({ required_error: 'El campo es requerido.' }),
-        code: z.string({ required_error: 'El campo es requerido.' }),
-        label: z.string({ required_error: 'El campo es requerido.' }),
-      },
-      { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
-    ),
-    eventoId: z
-      .array(
-        z.object({
-          value: z.number(),
-          code: z.string(),
-          label: z.string(),
-        }),
-      )
-      .optional(),
-    fechaHasta: z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-    fechaDesde: z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }),
-  })
-  .refine(({ fechaDesde, fechaHasta }) => fechaHasta == null || fechaDesde == null || fechaHasta >= fechaDesde, {
-    message: 'Fecha hasta debe ser mayor o igual a Fecha desde',
-    path: ['fechaHasta'],
-  });
+export const EventosClientesCreateSchema = z.object({
+  clienteId: z.object(
+    {
+      value: z.number({ required_error: 'El campo es requerido.' }),
+      code: z.string({ required_error: 'El campo es requerido.' }),
+      label: z.string({ required_error: 'El campo es requerido.' }),
+    },
+    { required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' },
+  ),
+  eventoId: z
+    .array(
+      z.object({
+        value: z.number(),
+        code: z.string(),
+        label: z.string(),
+      }),
+    )
+    .optional(),
+  rangoFechas: z
+    .array(z.date({ required_error: 'El campo es requerido.', invalid_type_error: 'El campo es requerido.' }))
+    .nullable(),
+});
 
 export type EventosClienteFormSchemaType = z.infer<typeof EventosClientesCreateSchema>;
