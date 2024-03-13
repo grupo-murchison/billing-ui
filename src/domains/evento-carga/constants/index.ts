34be6d3ada814b4414f72acee9373a9e48fb19fb
assertnano <112961459+assertnano@users.noreply.github.com>
2024-03-13 15:12:16 -0300

    Alta de eventos desde archivo excel (#140)
    * Incio Drag and Drop
    
    * EventoCarga ConfirmDialog
    
    * EventoCarga
    
    * EventoCarga
    
    * env test
    
    * env prd
    
    ---------
    
    Co-authored-by: Joel Imparado <joelimparado@gmail.com>

---
+++ src/domains/evento-carga/constants/index.ts
@@ -0,0 +1,5 @@
+import { BreadcrumbItem } from '@app/hocs';
+
+export const labelAndPath = { label: 'Evento Carga', path: '/evento-carga' };
+
+export const EventoCargaBreadcrumb: BreadcrumbItem[] = [labelAndPath];
